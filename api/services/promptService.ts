f4cdad1fd72c80c3ce146b4f8aa14c698a57fa71
Aditya Choudhary <adi@Adityas-MacBook-Air-2.local>
2024-12-03 03:45:08 +0530

    changes

--- api/services/promptService.ts
+++ api/services/promptService.ts
@@ -2,29 +2,23 @@ import prisma from '../db';
 
 interface PromptData {
   prompt: string;
-  output: string[];
+  output: string;
 }
 
 export async function savePrompt(data: PromptData): Promise<{ id: number; prompt: string; output: string } | null> {
   try {
-    if (!Array.isArray(data.output) || data.output.length === 0) {
-      throw new Error('Output must be a non-empty array of strings.');
-  }  
     const existingPrompt = await prisma.prompt.findUnique({
       where: { prompt: data.prompt },
     });
     if (existingPrompt) {
       return existingPrompt;
     }
-    if (!Array.isArray(data.output) || data.output.length === 0) {
-      throw new Error('Output must be a non-empty array of strings.');
-  }
   
 
     const newPrompt = await prisma.prompt.create({
       data: {
         prompt: data.prompt,
-        output: data.output[0], 
+        output: data.output, 
       },
     });
 
